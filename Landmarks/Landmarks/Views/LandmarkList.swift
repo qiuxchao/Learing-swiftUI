8a4a7949623559593d79425354ab889bb0efcc41
qiuxchao <2915241235@qq.com>
2023-09-03 15:59:40 +0800

    Set Up Navigation Between List and Detail 设置列表和详细信息之间的导航

--- Landmarks/Landmarks/Views/LandmarkList.swift
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -9,10 +9,18 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        // 动态渲染列表，列表结构有 Identifiable 协议时不需要指定第二个参数 id（唯一标识）
-        List(landmarks) { landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationView{
+            // 动态渲染列表，列表结构有 Identifiable 协议时不需要指定第二个参数 id（唯一标识）
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail()
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
         }
+        
     }
 }
 
