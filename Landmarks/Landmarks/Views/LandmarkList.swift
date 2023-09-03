1482ba0c5672977c38c196c089b92ab7b3c57e5e
qiuxchao <2915241235@qq.com>
2023-09-03 16:47:55 +0800

    Generate Previews Dynamically 动态生成预览

--- Landmarks/Landmarks/Views/LandmarkList.swift
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -26,6 +26,12 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        // 指定多个预览设备
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+                    LandmarkList()
+                        .previewDevice(PreviewDevice(rawValue: deviceName))
+                        .previewDisplayName(deviceName)
+                }
+
     }
 }
