1c9b7ac5abc00e6dc88ef90a006dc645b8006fcd
qiuxchao <2915241235@qq.com>
2023-10-13 22:20:42 +0800

    3. Layout code and Preview

--- c.d/c.d/ContentView.swift
+++ c.d/c.d/ContentView.swift
@@ -21,26 +21,35 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.bold)
             
-            Text("20 sections - 3 hours")
+            Text("20 sections - 3 hours".uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
+                .foregroundColor(.secondary)
             Text("Build an Ios app for Ios 16 with custom layout, animations and ...")
                 .font(.footnote)
                 .multilineTextAlignment(.leading)
                 .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.secondary)
             
         }
         .padding(.all, 20.0)
+        .padding(.vertical, 20)
         .frame(height: 350.0)
         .background(Color("Background"))
         .cornerRadius(30.0)
-        .shadow(radius: /*@START_MENU_TOKEN@*/20/*@END_MENU_TOKEN@*/)
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .padding(.horizontal, 20)
+        
+        // 注意：这里多次调用了 padding
+        // 第一次是给 VStack 设置四周 20 的内边距
+        // 第二次是给垂直方向再加 20 内边距
+        // 第三次会变成外边距（好像是放到背景色后面就会变成外边距）
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            
     }
 }
