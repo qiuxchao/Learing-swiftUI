ba36b88d76d4983b5090be30e1a8b2867d5090b5
qiuxchao <2915241235@qq.com>
2023-10-13 22:49:50 +0800

    4. Material and Foreground Style

--- c.d/c.d/ContentView.swift
+++ c.d/c.d/ContentView.swift
@@ -20,31 +20,50 @@ struct ContentView: View {
             Text("SwiftUI for IOS 16")
                 .font(.largeTitle)
                 .fontWeight(.bold)
+                .foregroundStyle(LinearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
             
             Text("20 sections - 3 hours".uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
-                .foregroundColor(.secondary)
+                .foregroundStyle(.secondary)
             Text("Build an Ios app for Ios 16 with custom layout, animations and ...")
                 .font(.footnote)
                 .multilineTextAlignment(.leading)
                 .lineLimit(2)
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .foregroundColor(.secondary)
+            // foregroundColor 和 foregroundStyle 的区别：
+            // foregroundColor 只能设置颜色
+            // foregroundStyle 可以设置更多样式，渐变、材质、多种颜色等
             
         }
         .padding(.all, 20.0)
         .padding(.vertical, 20)
         .frame(height: 350.0)
-        .background(Color("Background"))
+        .background(.ultraThinMaterial)
         .cornerRadius(30.0)
-        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .shadow(color: Color("Shadow")
+            .opacity(0.3), radius: 10, x: 0, y: 10)
         .padding(.horizontal, 20)
+        .background(
+            Image("Blob 1")
+                .offset(x: 250, y: -100)
+        )
+        .overlay(
+            Image("Illustration 5")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 230)
+                .offset(x: 32, y: -80)
+        )
         
         // 注意：这里多次调用了 padding
         // 第一次是给 VStack 设置四周 20 的内边距
         // 第二次是给垂直方向再加 20 内边距
         // 第三次会变成外边距（好像是放到背景色后面就会变成外边距）
+        
+        // background 和 overlay 的区别：
+        // background 在下面，overlay 覆盖在上面
     }
 }
 
