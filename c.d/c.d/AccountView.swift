5f3b54b122ee2cb4766cafefe64c1a460a6412f9
qiuxchao <2915241235@qq.com>
2023-10-15 11:11:08 +0800

    12. Graphics in Canvas(在画布中绘制图形)

--- c.d/c.d/AccountView.swift
+++ c.d/c.d/AccountView.swift
@@ -37,10 +37,7 @@ struct AccountView: View {
                 .padding()
                 .background(Circle().fill(.ultraThinMaterial))
                 .background(
-                    Image(systemName: "hexagon")
-                        .symbolVariant(.fill)
-                        .foregroundColor(.blue)
-                        .font(.system(size: 200))
+                    HexagonView()
                         .offset(x: -50, y: -100)
                 )
             Text("Qiu Xc")
